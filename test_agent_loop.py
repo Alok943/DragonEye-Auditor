eb8d12f3050a33cd6b0e3dfec39730d7557580dd
Alok Singh <aloksingh98541@gmail.com>
2026-04-08 04:38:03 +0530

    remove unnecessary test script

--- test_agent_loop.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open("ai_studio_code.txt") as f:
-    data = json.load(f)
-
-reviews = json.loads(data["response"])
-
-with open("reviews_v1.json", "w", encoding="utf-8") as f:
-    json.dump(reviews, f, ensure_ascii=False, indent=2)
